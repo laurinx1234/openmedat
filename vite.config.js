e741e1f5a891298785329a9eb9cd224359de510b
laurin <256870770+laurinx1234@users.noreply.github.com>
2026-06-24 14:36:08 +0200

    Update Vite config to include allowedHosts
    Added allowedHosts configuration for server and preview.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: { historyApiFallback: true },
+  server: { 
+    historyApiFallback: true,
+    allowedHosts: ['openmedat.rndserver.cc'] // Falls im Dev-Modus blockiert
+  },
+  preview: {
+    allowedHosts: ['openmedat.rndserver.cc'] // Falls im Preview-Modus blockiert
+  }
 })
